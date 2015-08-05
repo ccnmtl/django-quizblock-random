8a3e942b1992e7222c5ca105e86c4cdd14865092
Anders Pearson <anders@columbia.edu>
2015-08-05 12:39:07 +0200

    initial import

---
+++ runtests.py
@@ -0,0 +1,61 @@
+""" run tests for pagetree
+
+$ virtualenv ve
+$ ./ve/bin/pip install Django==1.8.1
+$ ./ve/bin/pip install -r test_reqs.txt
+$ ./ve/bin/python runtests.py
+"""
+from django.conf import settings
+from django.core.management import call_command
+import django
+
+
+def main():
+    # Dynamically configure the Django settings with the minimum necessary to
+    # get Django running tests
+    settings.configure(
+        INSTALLED_APPS=(
+            'django.contrib.auth',
+            'django.contrib.contenttypes',
+            'django.contrib.sessions',
+            'pagetree',
+            'django_markwhat',
+            'django_jenkins',
+            'quizblock',
+            'quizblock_random'
+        ),
+        TEST_RUNNER='django.test.runner.DiscoverRunner',
+        MIDDLEWARE_CLASSES=[],
+        JENKINS_TASKS = (
+            'django_jenkins.tasks.with_coverage',
+        ),
+        PROJECT_APPS = [
+            'quizblock_random',
+        ],
+        COVERAGE_EXCLUDES_FOLDERS = ['migrations', 'south_migrations'],
+        ROOT_URLCONF = [],
+        PAGEBLOCKS = ['pagetree.TestBlock', 'quizblock.Quiz'],
+        SOUTH_TESTS_MIGRATE=False,
+
+        # Django replaces this, but it still wants it. *shrugs*
+        DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': ':memory:',
+                'HOST': '',
+                'PORT': '',
+                'USER': '',
+                'PASSWORD': '',
+                }
+            }
+    )
+    try:
+        django.setup()
+    except AttributeError:
+        pass
+
+    # Fire off the tests
+    call_command('jenkins')
+
+if __name__ == '__main__':
+    main()
